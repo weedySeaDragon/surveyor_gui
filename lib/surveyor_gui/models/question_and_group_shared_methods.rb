d5bbd031c294bc3bab473298073aa59cc3cf80f2
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-06-11 01:52:12 -0700

    whitespace formatting

--- lib/surveyor_gui/models/question_and_group_shared_methods.rb
+++ lib/surveyor_gui/models/question_and_group_shared_methods.rb
@@ -4,12 +4,15 @@ module SurveyorGui
 
     module QuestionAndGroupSharedMethods
 
+
       def controlling_questions
+
         dependencies = []
         dependencies << self.dependency
         dependencies.map { |d| d.dependency_conditions.map { |dc| dc.question.part_of_group? ? dc.question.question_group.questions.last : dc.question } }.flatten.uniq
 
       end
+
 
     end
 
